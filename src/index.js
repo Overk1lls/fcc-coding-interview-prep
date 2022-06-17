466648e6aa35d31ecac180d3eda2aa2509990020
Yurii Pakhota <Ov3RforDream@gmail.com>
2022-06-17 17:10:07 +0300

    Inventory Update task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import sym from "./find-the-symmetric-difference/sym.js";
+import updateInventory from "./inventory-update/inventory.js";
 
-console.log("Find the Symmetric Difference:", sym([1, 2, 3], [5, 2, 1, 4]));
+console.log('Find the Symmetric Difference:', sym([1, 2, 3], [5, 2, 1, 4]));
+
+const curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+const newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+console.log('Inventory Update:', updateInventory(curInv, newInv));
---
+++ src/inventory-update/inventory.js
@@ -0,0 +1,21 @@
+/**
+ * Updates the example of a 2D inventory.
+ * @param {Record<number, string>[][]} inv Current inventory.
+ * @param {Record<number, string>[][]} newItems Inventory with items to update.
+ * @returns An updated inventory, sorted in alphabetical order.
+ */
+const updateInventory = (inv, newItems) => {
+    const result = [...inv];
+
+    newItems.forEach(newItem => {
+        const isItem = result.find(item => item[1] === newItem[1]);
+        if (isItem) {
+            result[result.indexOf(isItem)][0] += newItem[0];
+        } else {
+            result.push(newItem);
+        }
+    });
+    return result.sort((a, b) => a[1] > b[1] ? 1 : -1);
+};
+
+export default updateInventory;
