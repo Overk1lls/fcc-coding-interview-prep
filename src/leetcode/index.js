43469459c7356c1ddb3d4fe966e3c9fa5e8294ee
Yurii Pakhota <Ov3RforDream@gmail.com>
2022-11-11 14:54:16 +0200

    Do the prod consecutive Fib numbers exercise

--- src/leetcode/index.js
+++ src/leetcode/index.js
@@ -1,3 +1,4 @@
 
 export * from './human-readable-duration-format/index.js';
 export * from './valid-parenthesis/index.js';
+export * from './prod-consecutive-fin-nums/index.js';
---
+++ src/leetcode/prod-consecutive-fin-nums/index.js
@@ -0,0 +1,16 @@
+/**
+ * @example 4895 => [55, 89, true]
+ * @example 800 => [34, 55, false]
+ * @example 714 => [21, 34, true]
+ * @param {number} prod 
+ */
+export function productFib(prod) {
+  let n = 0;
+  let n1 = 1;
+
+  while (n * n1 < prod) {
+    n1 += n;
+    n = n1 - n;
+  }
+  return [n, n1, n * n1 === prod];
+}
