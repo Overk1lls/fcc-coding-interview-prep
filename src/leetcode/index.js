906a67b311d002c7b964ad09fd801716a53f293b
Yurii Pakhota <Ov3RforDream@gmail.com>
2022-11-18 20:30:36 +0200

    Do the Range Extraction exercise

--- src/leetcode/index.js
+++ src/leetcode/index.js
@@ -5,3 +5,4 @@ export * from './prod-consecutive-fin-nums/index.js';
 export * from './sum-strings-as-nums/index.js';
 export * from './primes-with-even-digits/index.js';
 export * from './nesting-structure-comparison/index.js';
+export * from './range-extraction/index.js';
---
+++ src/leetcode/range-extraction/index.js
@@ -0,0 +1,28 @@
+/**
+ * @example [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20] => '-10--8,-6,-3-1,3-5,7-11,14,15,17-20'
+ * @param {number[]} list 
+ */
+ function solution(list) {
+  /**
+   * @type {number[]}
+   */
+  let temp = [];
+  let result = '';
+
+  list.forEach((v, i) => {
+    temp.push(v);
+
+    if (list[i + 1] - v !== 1) {
+      if (result) {
+        result += ',';
+      }
+      if (temp.length >= 3) {
+        result += temp.shift() + '-' + temp.pop();
+      } else {
+        result += temp.join(',');
+      }
+      temp = [];
+    }
+  });
+  return result;
+}
